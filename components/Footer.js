2f9c67adfe41cc7a85e8ecc63d8c6ba1d1d61996
Emavu <62607846+Emavu@users.noreply.github.com>
2025-06-02 12:58:10 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -25,14 +25,14 @@ function Footer() {
                         <div data-name="footer-social">
                             <h3 className="font-bold mb-4">Follow Us</h3>
                             <div className="flex space-x-4">
-                                <a href="#" className="text-gray-600 hover:text-black text-xl">
-                                    <i className="fab fa-facebook"></i>
+                                <a href="https://www.instagram.com/_garmus/" className="text-gray-600 hover:text-black text-xl">
+                                    <i className="fab fa-instagram"></i>
                                 </a>
-                                <a href="#" className="text-gray-600 hover:text-black text-xl">
+                                <a href="https://www.linkedin.com/in/domantas-garmus-584b34294/?originalSubdomain=lt" className="text-gray-600 hover:text-black text-xl">
                                     <i className="fab fa-linkedin"></i>
                                 </a>
-                                <a href="#" className="text-gray-600 hover:text-black text-xl">
-                                    <i className="fab fa-instagram"></i>
+                                <a href="https://www.behance.net/domantasgarmus" className="text-gray-600 hover:text-black text-xl">
+                                    <i className="fab fa-behance"></i>
                                 </a>
                             </div>
                         </div>
